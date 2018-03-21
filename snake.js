1d89fddd04df022d7e1a56169a61d74dbe6c5a09
paga2004 <37170150+paga2004@users.noreply.github.com>
2018-03-21 20:03:34 +0100

    Add files via upload
    Removing smooth movement and playing around with colors.

--- snake.js
+++ snake.js
@@ -2,23 +2,25 @@ var canvasSize = (window.innerHeight < window.innerWidth ? window.innerHeight * 
 var fieldSize = 17;
 var grid = create2dArray(fieldSize,fieldSize);
 var r = canvasSize/fieldSize;
-var dir;
+var cdir, dir;
 var s = new Snake;
+
+
 function setup() {
 	createCanvas(canvasSize+1, canvasSize+1);
 	background(0);
-	
+	setFrameRate(5);
 }
 
 function draw() {
 	for(var i = 0; i < fieldSize; i++) {
 		for(var j = 0; j < fieldSize; j++) {
-			fill(50,200,50,200);
+			fill(0);
 			rect(i*r,j*r,r,r)
 		}
 	}
 	
-	fill(50,50,200);
+	fill(0,255,0);
 	rect(s.x*r,s.y*r,r,r);
 	
 	s.update();
@@ -27,22 +29,18 @@ function draw() {
 function keyPressed() {
 	if (keyCode === UP_ARROW) {
 		dir = 0;
-		s.fix();
 	}
 		
 	if (keyCode === RIGHT_ARROW) {
 		dir = 1;
-		s.fix();
 	}
 	
 	if (keyCode === DOWN_ARROW) {
 		dir = 2;
-		s.fix();
 	}
 	
 	if (keyCode === LEFT_ARROW) {
 		dir = 3;
-		s.fix();
 	}
 
 
@@ -55,24 +53,20 @@ function Snake() {
 	this.update = function() {
 		switch(dir) {
 			case 0: 
-				this.y-=0.1;
+				this.y--;
 				break;
 			case 1: 
-				this.x+=0.1;
+				this.x++;
 				break;
 			case 2: 
-				this.y+=0.1;
+				this.y++;
 				break;
 			case 3: 
-				this.x-=0.1;
+				this.x--;
 				break;
 		}
-	}
-	
-	this.fix = function() {
-		this.y = Math.round(this.y);
-		this.x = Math.round(this.x);
 
+		
 	}
 
 }
