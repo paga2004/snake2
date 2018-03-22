7ec97fcd6270a40f879d00dff87620f1ef226c75
paga2004 <37170150+paga2004@users.noreply.github.com>
2018-03-22 21:00:17 +0100

    Add files via upload
    Adding tail, food and other things.

--- snake.js
+++ snake.js
@@ -1,24 +1,23 @@
 var canvasSize = (window.innerHeight < window.innerWidth ? window.innerHeight * 0.98 : window.innerWidth * 0.98);
 var fieldSize = 17;
-var grid = create2dArray(fieldSize,fieldSize);
 var r = canvasSize/fieldSize;
-var dir;
-var s = new Snake;
+var dir, score = 1;
 
+var s = new Snake, f = new Food;
 
 function setup() {
 	createCanvas(canvasSize, canvasSize);
-	background(0);
 	setFrameRate(5);
+	
 }
 
 function draw() {
-
-	
-	fill(0,255,0);
-	rect(s.x*r,s.y*r,r,r);
-	
+	background(0);
 	s.update();
+	
+	s.show();
+	f.show();
+	
 }
 
 function keyPressed() {
@@ -38,12 +37,12 @@ function keyPressed() {
 		dir = 3;
 	}
 
-
 }
 
 function Snake() {
 	this.x = Math.floor(fieldSize/2);
 	this.y = Math.floor(fieldSize/2);
+	this.tail = [];
 	
 	this.update = function() {
 		switch(dir) {
@@ -61,8 +60,76 @@ function Snake() {
 				break;
 		}
 
+		if(this.x < 0 || this.x > fieldSize -1  || this.y < 0 || this.y > fieldSize - 1)
+			this.kill();
 		
+		if(this.x == f.x && this.y == f.y)
+			this.eat();
+		
+		
+		if(this.tail.length == score) {
+			for(var i=0; i < this.tail.length -1; i++) {
+				this.tail[i] = this.tail[i+1]
+			}
+		}
+		this.tail[score - 1]= [this.x, this.y];
+	
+		for(var i = 0; i < this.tail.length -1 ; i++) {
+			if(this.tail[i][0] == this.x && this.tail[i][1] == this.y)
+				this.kill();
+		}
+
 	}
 
+	this.show = function() {
+		fill(0,255,0);
+		rect(this.x*r,this.y*r,r,r);
+		
+		
+		
+		for(var i = 0; i < this.tail.length; i++) {
+			rect(this.tail[i][0] * r, this.tail[i][1] * r,r,r);
+		}
+	}
+	
+	
+	this.kill = function() {
+		this.x = Math.floor(fieldSize/2);
+		this.y = Math.floor(fieldSize/2);
+		score = 1;
+		dir = 42;
+		this.tail = [];
+		f.newPos();
+		
+	}
+	
+	this.eat = function() {
+		score++;
+		f.newPos();
+	}
+}
 
+function Food() {
+	this.x = Math.round(Math.random() * (fieldSize - 1));
+	this.y = Math.round(Math.random() * (fieldSize - 1));
+	
+	this.newPos = function() {
+		do{
+			this.x = Math.floor(Math.random() * (fieldSize - 1));
+			this.y = Math.floor(Math.random() * (fieldSize - 1));
+		}while(!this.legalPos)
+	}
+
+	this.show = function() {
+		fill(255,0,0);
+		rect(this.x*r,this.y*r,r,r);
+	}
+	
+	this.legalPos = function() {
+		for(var i = 0; i < s.tail.length; i++) {
+			if(this.x == s.tail[i][0] && this.y == s.tail[i][1])
+				return false;
+			}
+		return true;
+	}
 }
